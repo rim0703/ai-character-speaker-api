d11dda18efff69d04e432b22f6e51dbe7e3c11cc
rim0703 <riiim0703@gmail.com>
2024-07-23 11:53:40 +0900

    [Feature] Hardware 파일 감지 코드 추가

--- Hardware/speaker.py
+++ Hardware/speaker.py
@@ -1,28 +1,37 @@
 import os
 from src.deviceIPCheck import deviceIPcheck
-from src.deviceCharacter import showCharacter, closeWindow
+from src.deviceCharacter import showCharacter
 from src.deviceVoice import playVoice
+from preferredsoundplayer import playsound
+import time
 
-# Dummy Test
-character = "jjangu"
-image_path = "./img/" + character + ".png"
-wav_file_path = "./voice/" + character + ".wav"
 ip_reader = "ip.mp3"
+directory_path = "/home/willtek/lge/voice"
 
 connected = False
 if __name__ == "__main__":
+    default_image_path = "./img/default.png"
+    showCharacter(default_image_path)
+
     if not connected:
-        default_image_path = "./img/default.png"
-        showCharacter(default_image_path)
         deviceIPcheck()
 
         while os.path.exists(ip_reader):
             connected = False
+            time.sleep(3)  # 3초에 한번 출력
+            playsound(ip_reader)
         connected = True
-        closeWindow()
 
-    if connected:
-        # TODO: Receive converted voice file
-        showCharacter(image_path)
-        playVoice(wav_file_path)
-        closeWindow()
+    while connected:
+        time.sleep(3)
+        if any(
+            os.path.isfile(os.path.join(directory_path, f))
+            for f in os.listdir(directory_path)
+        ):
+            for f in os.listdir(directory_path):
+                voice_path = os.path.join(directory_path, f)
+                playVoice(voice_path)
+                os.remove(voice_path)
+                break
+        else:
+            print("[NO VOICE] waiting for file...")
